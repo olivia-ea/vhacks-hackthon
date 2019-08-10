1f918868fc0b11c26cf570a35f9bdc0317d5dff9
Olivia Ea <ea.olivia1@gmail.com>
2019-08-10 14:51:37 -0700

    Testing post request

--- views.py
+++ views.py
@@ -5,13 +5,64 @@ from jinja2 import StrictUndefined
 # import requests
 # from model import 
 
-
 app = Flask(__name__)
 app.jinja_env.undefinded = StrictUndefined
 app.debug = True
+
 
 @app.route('/')
 def index():
     """ Homepage """
 
+    # has description of what the app does
+    # displays returning user or new user routes
+
     return render_template('homepage.html')
+
+@app.route('/login-verification', methods=["GET"])
+def returning_user_login():
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    try:
+        user = Seeking_Help_User.query.filter(Seeking_Help_User.user_id==username).first()
+    except:
+        pass
+
+    try:
+        user = Looking_To_Help_User.query.filter(Looking_To_Help_User.user_id==username).first()
+    except:
+        pass
+
+    # how to handle if user has both accounts?
+
+    if user:
+        if user.password == password:
+            user_id = user.user_id
+            return redirect("dashboard.html")
+        else:
+            return("The password is incorrect.")
+    else:
+        return render_template('homepage.html')
+        
+    return render_template('returning_user.html')
+
+
+@app.route('/new-user-login')
+def new_user_login():
+
+    # are you seeking help or looking to help => anchor to bottom to registration form
+
+    return render_template('new_user.html')
+
+
+
+
+
+
+
+
+
+
+
